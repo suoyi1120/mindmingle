a4a51c2dd0a3ec562610018deb98029956dfec6a
Fengqi Jia <fjia0419@uni.sydney.edu.au>
2025-04-06 20:18:18 +1000

    update community post

---
+++ src/main/java/com/group02/mindmingle/repository/CommunityPostRepository.java
@@ -0,0 +1,13 @@
+package com.group02.mindmingle.repository;
+
+import com.group02.mindmingle.model.CommunityPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long> {
+    List<CommunityPost> findByIsVisibleTrueOrderByCreatedAtDesc();
+    List<CommunityPost> findByAuthorId(Long userId);
+}
---
+++ src/main/java/com/group02/mindmingle/service/CommunityPostService.java
@@ -0,0 +1,15 @@
+package com.group02.mindmingle.service;
+
+import com.group02.mindmingle.model.CommunityPost;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommunityPostService {
+    CommunityPost createPost(CommunityPost post);
+    List<CommunityPost> getAllVisiblePosts();
+    Optional<CommunityPost> getPostById(Long id);
+    CommunityPost updatePost(Long id, CommunityPost updatedPost);
+    void deletePost(Long id);
+    List<CommunityPost> getPostsByUser(Long userId);
+}
