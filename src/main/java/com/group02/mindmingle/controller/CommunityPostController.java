7141794cbbec445d4c140a3b0df98d772728f342
Fengqi Jia <fjia0419@uni.sydney.edu.au>
2025-04-17 22:16:31 +1000

    Update CommunityPostController.java

--- src/main/java/com/group02/mindmingle/controller/CommunityPostController.java
+++ src/main/java/com/group02/mindmingle/controller/CommunityPostController.java
@@ -4,7 +4,7 @@ import com.group02.mindmingle.model.CommunityPost;
 import com.group02.mindmingle.service.CommunityPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +16,20 @@ public class CommunityPostController {
     @Autowired
     private CommunityPostService postService;
 
+    // Add a community post
     @PostMapping("/posts")
-    // @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CommunityPost> createPost(@RequestBody CommunityPost post) {
         return ResponseEntity.ok(postService.createPost(post));
     }
 
+    // Check all community posts
     @GetMapping("/posts")
     public ResponseEntity<List<CommunityPost>> getAllVisiblePosts() {
         return ResponseEntity.ok(postService.getAllVisiblePosts());
     }
 
+    // check a post by id
     @GetMapping("/posts/{id}")
     public ResponseEntity<CommunityPost> getPostById(@PathVariable Long id) {
         return postService.getPostById(id)
@@ -34,14 +37,16 @@ public class CommunityPostController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    // edit a post by id
     @PutMapping("/posts/{id}")
-    // @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CommunityPost> updatePost(@PathVariable Long id, @RequestBody CommunityPost post) {
         return ResponseEntity.ok(postService.updatePost(id, post));
     }
 
+    //delete a post by id
     @DeleteMapping("/posts/{id}")
-    // @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletePost(@PathVariable Long id) {
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
